786a9142f64ee0bcbe6fde4f5e3aadecf45e71a3
grigosoft <grig.griganto@gmail.com>
2020-07-07 09:09:56 +0200

    controller

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -6,9 +6,66 @@ class TradesController < ApplicationController
   def index
     @trades = Trade.all
   end
-  
+
   def filtro
-    @trades = Trade.all
+    # Initialize filterrific with the following params:
+    # * `Student` is the ActiveRecord based model class.
+    # * `params[:filterrific]` are any params submitted via web request.
+    #   If they are blank, filterrific will try params persisted in the session
+    #   next. If those are blank, too, filterrific will use the model's default
+    #   filter settings.
+    # * Options:
+    #     * select_options: You can store any options for `<select>` inputs in
+    #       the filterrific form here. In this example, the `#options_for_...`
+    #       methods return arrays that can be passed as options to `f.select`
+    #       These methods are defined in the model.
+    #     * persistence_id: optional, defaults to "<controller>#<action>" string
+    #       to isolate session persistence of multiple filterrific instances.
+    #       Override this to share session persisted filter params between
+    #       multiple filterrific instances. Set to `false` to disable session
+    #       persistence.
+    #     * default_filter_params: optional, to override model defaults
+    #     * available_filters: optional, to further restrict which filters are
+    #       in this filterrific instance.
+    #     * sanitize_params: optional, defaults to `true`. If true, all filterrific
+    #       params will be sanitized to prevent reflected XSS attacks.
+    # This method also persists the params in the session and handles resetting
+    # the filterrific params.
+    # In order for reset_filterrific to work, it's important that you add the
+    # `or return` bit after the call to `initialize_filterrific`. Otherwise the
+    # redirect will not work.
+    @filterrific = initialize_filterrific(
+      Trade,
+      params[:filterrific],
+      select_options: {
+        sorted_by: Trade.options_for_sorted_by,
+        with_country_id: Trade.options_for_select,
+      },
+      persistence_id: "shared_key",
+      default_filter_params: {},
+      available_filters: [:sorted_by],
+      sanitize_params: true,
+    ) || return
+    # Get an ActiveRecord::Relation for all students that match the filter settings.
+    # You can paginate with will_paginate or kaminari.
+    # NOTE: filterrific_find returns an ActiveRecord Relation that can be
+    # chained with other scopes to further narrow down the scope of the list,
+    # e.g., to apply permissions or to hard coded exclude certain types of records.
+    @trades = @filterrific.find.page(params[:page])
+
+    # Respond to html for initial page load and to js for AJAX filter updates.
+    respond_to do |format|
+      format.html
+      format.js
+    end
+
+  # Recover from invalid param sets, e.g., when a filter refers to the
+  # database id of a record that doesn’t exist any more.
+  # In this case we reset filterrific and discard all filter params.
+  rescue ActiveRecord::RecordNotFound => e
+    # There is an issue with the persisted param_set. Reset it.
+    puts "Had to reset filterrific params: #{e.message}"
+    redirect_to(reset_filterrific_url(format: :html)) && return
   end
 
   # GET /trades/1
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -7,4 +7,51 @@ class Trade < ApplicationRecord
   has_one_attached :screen_sentiment_h1
 
 
+
+    # db columns:
+    # integer: id
+    # string: first_name
+    # string: last_name
+    # text: email
+    # integer: country_id
+    # datetime: created_at
+
+    # This directive enables Filterrific for the Student class.
+    # We define a default sorting by most recent sign up, and then
+    # we make a number of filters available through Filterrific.
+    filterrific(
+      default_filter_params: { sorted_by: "data_micro_desc" },
+      available_filters: [
+        :sorted_by,
+        :search_query,
+        :with_country_id,
+        :with_created_at_gte,
+      ],
+    )
+
+    # ActiveRecord association declarations
+    belongs_to :country
+
+    # Scope definitions. We implement all Filterrific filters through ActiveRecord
+    # scopes. In this example we omit the implementation of the scopes for brevity.
+    # Please see 'Scope patterns' for scope implementation details.
+    scope :search_query, ->(query) {
+      # Filters students whose name or email matches the query
+    }
+    scope :sorted_by, ->(sort_key) {
+      # Sorts students by sort_key
+    }
+    scope :with_data_micro_gte, ->(ref_date) {
+    }
+
+    # This method provides select options for the `sorted_by` filter select input.
+    # It is called in the controller as part of `initialize_filterrific`.
+    def self.options_for_sorted_by
+      [
+        ["Symbol (a-z)", "symbol_asc"],
+        ["Registration date (newest first)", "data_micro_desc"],
+        ["Registration date (oldest first)", "data_micro_asc"]
+      ]
+    end
+
 end
